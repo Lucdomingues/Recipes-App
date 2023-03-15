ed91c93910fab1dcb14723821c51c52b0e468af8
Ludomingues <106187863+Lucdomingues@users.noreply.github.com>
2023-03-15 18:11:40 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import AppProvider from './context/Provider';
@@ -8,9 +7,7 @@ import AppProvider from './context/Provider';
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </BrowserRouter>,
   );
